c0d3b85db4740ede00b48eed3e24f1d683a2e750
Imran Hasan <imran.h94@gmail.com>
2024-03-14 02:30:40 +0500

    made fixes to Header and CoreConcept import statements

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,8 @@
 import reactImg from './assets/react-core-concepts.png'
 
 import { CORE_CONCEPTS } from './data'
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
-
-function genRandomInt(max) {
- }
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(3)]
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  )
-}
-
-function CoreConcept({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
+import Header from './components/Header/Header'
+import CoreConcept from './components/CoreConcept';
 
 function App() {
   return (
